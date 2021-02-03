15833923a7a071d9cff617d838ab7e85a4ca4133
hoan.lee <lonelywolf.lee@gmail.com>
2021-02-03 17:38:17 +0900

    Add space for request/response

--- frontend/src/views/Api.vue
+++ frontend/src/views/Api.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="api">
+    <v-row>
+      <v-col class="request" cols="12" sm="6">
+        <v-card>
+          <v-card-title>REQUEST</v-card-title>
+          <v-card-text>
+
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col class="response" cols="12" sm="6">
+        <v-card>
+          <v-card-title>RESPONSE</v-card-title>
+          <v-card-text>
+
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
+
+<style scoped>
+.api {
+  margin: 32px;
+}
+.request {
+  outline-style: auto;
+}
+.response {
+  outline-style: auto;
+}
+</style>
